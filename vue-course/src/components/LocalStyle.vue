45a8db2db256dd6671506491e08b5853a0b6733e
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-21 14:28:10 +0800

    update

---
+++ vue-course/src/components/LocalStyle.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2 class="g-1">This is children component</h2>
+</template>
---
+++ vue-course/src/components/ModuleStyle.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 :class="$style['m-class']">Using Module CSS with $ syntax (habak-case)</h1>
+</template>
+
+<style module>
+/* kabab-case */
+.m-class {
+    background: firebrick;
+    color: white;
+}
+</style>
