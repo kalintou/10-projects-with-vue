b77190699cbaf6bef116e6f5164e417e7bd04ec1
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-21 14:31:57 +0800

    update

--- vue-course/src/components/ModuleStyle.vue
+++ vue-course/src/components/ModuleStyle.vue
@@ -1,11 +1,17 @@
 <template>
-    <h1 :class="$style['m-class']">Using Module CSS with $ syntax (habak-case)</h1>
+    <h1 :class="myClasses['m-class']">Using Module CSS with $ syntax (habak-case)</h1>
+    <h1 :class="myClasses.anotherClass">Using Module CSS with $ syntax ()</h1>
 </template>
 
-<style module>
+<style module="myClasses">
 /* kabab-case */
 .m-class {
     background: firebrick;
+    color: white;
+}
+
+.anotherClass {
+    background: seagreen;
     color: white;
 }
 </style>
