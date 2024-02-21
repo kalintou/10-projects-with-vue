41a6bef5be96a6d99ed2b60c2457697bb6e86cb4
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-21 14:53:02 +0800

    update

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,14 +1,8 @@
 <script setup>
-import GlobalStyle from './components/GlobalStyle.vue'
-import LocalStyle from './components/LocalStyle.vue'
-import CombinedStyle from './components/CombinedStyle.vue'
-import ModuleStyle from './components/ModuleStyle.vue';
+import MyEvent from './components/MyEvent.vue';
 </script>
 
 
 <template>
-  <GlobalStyle />
-  <LocalStyle />
-  <CombinedStyle />
-  <ModuleStyle />
+  <MyEvent />
 </template>
---
+++ vue-course/src/components/MyEvent.vue
@@ -0,0 +1,7 @@
+<script setup>
+const showMessage = (message) => alert(message)
+</script>
+
+<template>
+    <button @click="showMessage('This is my message')">Show Message</button>
+</template>
