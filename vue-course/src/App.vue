c34a171b7dbec5251d430714946587b89a95fb93
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-22 18:53:03 +0800

    update

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
+import BasicRef from './components/BasicRef.vue';
 
-//import BasicComponent from './components/BasicComponent.vue';
-//import ReactiveObject from './components/ReactiveObject.vue';
-import MultipleSources from './components/MultipleSources.vue';
 </script>
 
 <template>
-
-  <!-- <BasicComponent /> -->
-  <!-- <ReactiveObject /> -->
-  <MultipleSources />
+  <BasicRef />
 </template>
---
+++ vue-course/src/components/BasicRef.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {onMounted, ref} from 'vue';
+const myRef = ref('')
+console.log(myRef.value)
+
+onMounted(() => {
+    console.log(myRef.value)
+    myRef.value.focus()
+    myRef.value.value = 'something else'
+})
+</script>
+
+
+<template>
+    <div>
+        <input type="text" placeholder="Enter your name" ref="myRef">
+    </div>
+</template>
