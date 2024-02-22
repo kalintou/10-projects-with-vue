60359d5e2515c7425dac9924f182add21f18fce0
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-22 17:04:26 +0800

    update

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,49 +1,13 @@
 <script setup>
-import {provide} from 'vue'
-import PracticeComponent from './components/PracticeComponent.vue';
 
-provide('friends', ['kan' ,'jordan','HuXn','Jogn'])
-provide('games', {
-  id: 1,
-  title: "Epic adad",
-  genre: ['Action','Adventrue', 'RPG'],
-  platform: ['Windows','Mac']
-})
-
-provide('moreGames', [
-{
-    title: 'Space 1',
-    genre: 'SF',
-    platform: 'PC',
-    releaseDate: '1999'
-  },
-  {
-    title: 'Space 2',
-    genre: 'SFF',
-    platform: 'PC',
-    releaseDate: '1999'
-  },
-  {
-    title: 'Space 3',
-    genre: 'SFFF',
-    platform: 'PC',
-    releaseDate: '1999'
-  },
-  {
-    title: 'Space 3',
-    genre: 'SFFF',
-    platform: 'PC',
-    releaseDate: '1999'
-  }
-])
-
+import LifeCycleComponent from './components/LifeCycleComponent.vue';
+import {ref} from 'vue'
+const showHide = ref(true)
 </script>
-
-
-
 
 <template>
 
-  <PracticeComponent />
+  <LifeCycleComponent v-if="showHide" />
+  <button @click="showHide = !showHide">show / Hide</button>
 
 </template>
