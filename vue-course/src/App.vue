7c6084a6b72fe6d098b96cb618b502d1bac68a13
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-22 15:06:19 +0800

    update

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,8 +1,32 @@
 <script setup>
-import IterationComponent from './components/IterationComponent.vue';
+import {ref} from 'vue'
+const firstName = ref('HuXn')
+const lastName = ref('WebDev')
+const myFavoriteNumber = ref(7)
+
+import StaticPropsComponents from './components/StaticPropsComponents.vue';
+import DynamicPropsComponent from './components/DynamicPropsComponent.vue';
+import ChangingPropsValue from './components/ChangingPropsValue.vue';
+import PropValidation from './components/PropValidation.vue';
+import ComplexProps from './components/ComplexProps.vue';
+import CustomValidator from './components/CustomValidator.vue';
 </script>
 
 
 <template>
-  <IterationComponent />
+  <!-- <StaticPropsComponents name="Jordan"/>
+  <hr>
+  <DynamicPropsComponent :firstName="firstName" :lastName="lastName"/>
+  <hr>
+  <ChangingPropsValue :myFavoriteNumber="myFavoriteNumber"/>
+  <hr>
+  <PropValidation fullName="HuXn WebDev" :age="20"/>
+  <hr>
+  <ComplexProps 
+    :friends="['alex','john','kan','johnson']" 
+    :userInfo="{name: 'alex', age: 20, location: ['Earth', 'IDK']}"
+  />
+  <hr> -->
+  <CustomValidator name="Kan" :age="20" password="12345678password"/>
+  <hr>
 </template>
---
+++ vue-course/src/components/ChangingPropsValue.vue
@@ -0,0 +1,8 @@
+<script setup>
+const myFavoriteNumberProp = defineProps(['myFavoriteNumber'])
+
+</script>
+
+<template>
+    <h1>{{ myFavoriteNumberProp.myFavoriteNumber }}</h1>
+</template>
