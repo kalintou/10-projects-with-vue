7ac543400130c5ecf169d1810c20350b65ec816c
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-22 18:26:51 +0800

    update

--- vue-course/src/components/MultipleSources.vue
+++ vue-course/src/components/MultipleSources.vue
@@ -1,8 +1,19 @@
 <script setup>
+import {ref, watch} from 'vue'
+const username = ref('jordan')
+const count = ref(0)
 
+watch([username, count], (newVal, oldVal) => {
+    console.log('New Value', newVal)
+    console.log('Old Value', oldVal)
+}) 
 </script>
 
 
 <template>
-    <h1>faew</h1>
+    <h1>{{ username }}</h1>
+    <h1>Count: {{ count }}</h1>
+    
+    <button @click="username = 'Kan'">Change Name</button>
+    <button @click="count++">Increment Counter</button>
 </template>
