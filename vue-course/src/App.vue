480e04b438045a5391242584a544f982c3ec1266
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-22 22:08:25 +0800

    update

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import GettingDataWithFetch from './components/GettingDataWithFetch.vue';
-
+//import GettingDataWithFetch from './components/GettingDataWithFetch.vue';
+import GettingDataWithAxios from './components/GettingDataWithAxios.vue';
 
 </script>
 
 <template>
 
-  <GettingDataWithFetch />
+  <!-- <GettingDataWithFetch /> -->
+  <GettingDataWithAxios />
 
 </template>
