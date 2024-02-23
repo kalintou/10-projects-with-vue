c409264542afb6a3de8f6b93b7a205e519f82a5b
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-23 18:08:49 +0800

    update

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
-import TodoList from './1. Todo List/TodoList.vue';
-import RandomQuoteGenerator from './2. Random Quote Generator/RandomQuoteGenerator.vue'
-import AmazingCalculator from './3. Calculator/AmazingCalculator.vue';
+//import TodoList from './1. Todo List/TodoList.vue';
+//import RandomQuoteGenerator from './2. Random Quote Generator/RandomQuoteGenerator.vue'
+//import AmazingCalculator from './3. Calculator/AmazingCalculator.vue';
+import AccordionComponent from './4. Accordion/AccordionComponent.vue'
+
 
 </script>
 
 <template>
-  <TodoList />
-  <hr>
-  <RandomQuoteGenerator />
-  <hr>
-  <AmazingCalculator />
+  <AccordionComponent />
 
 </template>
