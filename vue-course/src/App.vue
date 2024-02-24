5f00c3ce37dd271c2b2f378f0f1e682a01f87271
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-24 11:38:07 +0800

    update

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
-//import TodoList from './1. Todo List/TodoList.vue';
-//import RandomQuoteGenerator from './2. Random Quote Generator/RandomQuoteGenerator.vue'
-//import AmazingCalculator from './3. Calculator/AmazingCalculator.vue';
-//import AccordionComponent from './4. Accordion/AccordionComponent.vue'
-//import FormValidation from './5. FormValidation/FormValidation.vue'
-//import PasswordGenerator from './6. PasswordGenerator/PasswordGenerator.vue'
-//import ProgressSteps from './7. Progress Steps/ProgressSteps.vue'
-//import DadJokes from './8. Dad Jokes/DadJokes.vue'
-//import GithubUsersSearch from './9. Github Users Search/GithubUsersSearch.vue'
+import TodoList from './1. Todo List/TodoList.vue';
+import RandomQuoteGenerator from './2. Random Quote Generator/RandomQuoteGenerator.vue'
+import AmazingCalculator from './3. Calculator/AmazingCalculator.vue';
+import AccordionComponent from './4. Accordion/AccordionComponent.vue'
+import FormValidation from './5. FormValidation/FormValidation.vue'
+import PasswordGenerator from './6. PasswordGenerator/PasswordGenerator.vue'
+import ProgressSteps from './7. Progress Steps/ProgressSteps.vue'
+import DadJokes from './8. Dad Jokes/DadJokes.vue'
+import GithubUsersSearch from './9. Github Users Search/GithubUsersSearch.vue'
 import WikiClone from './10. WikiClone/WikiClone.vue'
 
 
 </script>
 
 <template>
-  <!-- 
+  
   <TodoList />
   <RandomQuoteGenerator />
   <AmazingCalculator />
@@ -24,7 +24,7 @@ import WikiClone from './10. WikiClone/WikiClone.vue'
   <ProgressSteps />
   <DadJokes />
   <GithubUsersSearch />
-  -->
+ 
   <WikiClone />
 
 </template>
