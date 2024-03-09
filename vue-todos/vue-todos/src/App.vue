f450a703674ad4bac8a94b49fc87b7d6f47aea60
kan5470 <monkeyboyer.ks@gmail.com>
2024-03-09 15:42:26 +0800

    update

---
+++ vue-todos/vue-todos/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap')
+
+</style>
